47d984ef8da8ef76458623aa26fdcc2cfb2a08c0
gustavo dos Santos VItali Macedo <gugavitali2004@hotmail.com>
2026-05-10 01:37:26 -0300

    first commit

---
+++ api/src/main/java/me/dio/votacao_bbb_api/VotacaoBbbApiApplication.java
@@ -0,0 +1,13 @@
+package me.dio.votacao_bbb_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotacaoBbbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotacaoBbbApiApplication.class, args);
+	}
+
+}
